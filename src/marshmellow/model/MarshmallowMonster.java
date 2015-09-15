ca5e824fd426e2cb224206b7e7c1bd9656498b96
Wellrabbit <emilyjorton@gmail.com>
2015-09-15 13:00:30 -0600

    Added getters and Setters

--- src/marshmellow/model/MarshmallowMonster.java
+++ src/marshmellow/model/MarshmallowMonster.java
@@ -25,6 +25,71 @@ public class MarshmallowMonster
 		
 	}
 	
+	public String getMonsterName()
+	{
+		return MonsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return MonsterEyes;
+	}
+	
+	public int getMonsterEars()
+	{
+		return MonsterEars;
+	}
+	
+	public double getMonsterLegs()
+	{
+		return MonsterLegs;
+	}
+	
+	public double getMonsterHair()
+	{
+		return MonsterHair;
+	}	
+	
+	public boolean getMonsterAntena()
+	{
+		return MonsterAntena;
+	}
+	
+	public void setMonsterName(String MonsterName)
+	{
+		this.MonsterName = MonsterName;
+	}
+	
+	public void setMonsterEyes(int MonsterEyes)
+	{
+		this.MonsterEyes = MonsterEyes;
+	}
+	
+	public void setMonsterEars(int MonsterEars)
+	{
+		this.MonsterEars = MonsterEars;
+	}
+	
+	public void setMonsterHair(int MonsterHair)
+	{
+		this.MonsterHair = MonsterHair;
+	}
+	
+	public void setMonsterLegs(double MonsterLegs)
+	{
+		this.MonsterLegs = MonsterLegs;
+	}	
+	
+	public void setMonsterHair(double MonsterHair)
+	{
+		this.MonsterHair = MonsterHair;
+	}
+	
+	public void setMonsterAntena(boolean MonsterAntena)
+	{
+		this.MonsterAntena = MonsterAntena; 
+	}
+	
 	public String toString()
 	{
 		String monsterInfo = "My monster Has "+  MonsterLegs  + " eyes and its name is " +  MonsterName + " he has " + MonsterHair + " Hair and "+ MonsterEars 
